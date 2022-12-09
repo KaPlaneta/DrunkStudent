d8a0786cbec7becad22892220e874a3472cbf3e2
KaPlaneta <planeta.karolinaa@gmail.com>
2022-12-09 13:55:11 +0100

    feat: add pickers with alcohol, function of pure alcohol consumption, fix useState, add determining the current time, counting amount of pure alcohol in array,change button

--- SecondScreen.js
+++ SecondScreen.js
@@ -10,47 +10,67 @@ import {
   Button,
 } from 'react-native';
 import {Picker} from '@react-native-picker/picker';
-import {ThemedButton} from 'react-native-really-awesome-button';
 import moment from 'moment';
 
+const drinking = {
+  vodka: {
+    name: 'VODKA',
+    models: ['25ml', '40ml', '100ml', '200ml'],
+  },
+  beer: {
+    name: 'BEER',
+    models: ['250ml', '300ml', '500ml'],
+  },
+  wine: {
+    name: 'WINE',
+    models: ['250ml', '300ml', '500ml'],
+  },
+};
+
+const percent = {
+  vodka: 0.4,
+  beer: 0.05,
+  wine: 0.14,
+};
+
+function getPureAlcohol(capacity, drink) {
+  return parseInt(drinking[drink].models[capacity]) * percent[drink];
+}
+
 function SecondScreen({route, navigation}) {
-  const [drink, setdrink] = React.useState('vodka');
-  const [capacity, setcapacity] = React.useState(1);
-  // console.log(drink);
+  const [date, setDate] = React.useState([]);
+  const [pureAlcohol, setPureAlcohol] = React.useState([]);
+
+  const [drink, setDrink] = React.useState('vodka');
+  const [capacity, setCapacity] = React.useState(0);
   const make = drinking[drink];
   const selectionString = make.name + ' ' + make.models[capacity];
 
-  function getCurrentDate() {
-    var date = moment().utcOffset('+05:30').format('YYYY-MM-DD hh:mm:ss a');
-    console.log(date);
-    console.log(drink);
-    console.log(capacity);
-  }
+  const getCurrentDate = () => {
+    const currentDate = moment().utcOffset('+0').format('DD.MM hh:mm a');
+
+    const currentPureAlcohol = getPureAlcohol(capacity, drink);
+    setDate([...date, currentDate]);
+    console.log(pureAlcohol);
+
+    let lastElement = pureAlcohol[pureAlcohol.length - 1]; //last element in array
+    if (pureAlcohol.length == 0) {
+      lastElement = 0;
+    }
+    setPureAlcohol([...pureAlcohol, lastElement + currentPureAlcohol]);
+  };
 
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView>
         <View style={styles.container}>
-          {/* <Text>Choose drink:</Text>
-          <Picker
-            selectedValue={selectedValue}
-            style={{width: 150}}
-            onValueChange={(itemValue, itemIndex) =>
-              setSelectedValue(itemValue)
-            }>
-            <Picker.Item label="1/4" value="1/4" />
-            <Picker.Item label="2/4" value="2/4" />
-            <Picker.Item label="3/4" value="3/4" />
-            <Picker.Item label="4/4" value="4/4" />
-          </Picker> */}
-
           <Text>Choose drink:</Text>
           <Picker
             style={{width: 200}}
             selectedValue={drink}
             onValueChange={value => {
-              setdrink(value);
-              setcapacity(0);
+              setDrink(value);
+              setCapacity(0);
             }}>
             {Object.keys(drinking).map(value => (
               <Picker.Item
@@ -65,7 +85,7 @@ function SecondScreen({route, navigation}) {
             style={{width: 200}}
             selectedValue={capacity}
             key={drink}
-            onValueChange={value => setcapacity(value)}>
+            onValueChange={value => setCapacity(value)}>
             {drinking[drink].models.map((modelName, value) => (
               <Picker.Item
                 key={`${drink}_${value}`}
@@ -74,50 +94,33 @@ function SecondScreen({route, navigation}) {
               />
             ))}
           </Picker>
-          <ThemedButton
-            name="bruce"
-            type="secondary"
-            // backgroundColor="#a82f7a"
-            // textColor="#FFFFFF"
-            activityColor="#FFFFFF"
-            style={styles.button}
-            onPress={() => getCurrentDate()}>
-            ADD
-          </ThemedButton>
+          <Button
+            title="ADD"
+            color="#70431d"
+            onPress={() => getCurrentDate()}
+          />
 
-          <ThemedButton
-            name="bruce"
-            type="primary"
-            // backgroundColor="#a82f7a"
-            // textColor="#FFFFFF"
-            activityColor="#FFFFFF"
-            style={styles.button}
+          <Text>You selected: {selectionString}</Text>
+          <Text style={[{color: '#e3d9af'}]}>
+            You
+            selectedjdsbkjbdskjbkbsdkbckjsdkjvckjsdbcjkkdjscdkjjdsjcjbjcbcckcbjkc
+          </Text>
+
+          <Button
+            title="END PARTY"
+            color="#70431d"
             onPress={() =>
               navigation.navigate('ThirdScreen', {
-                capacity: make.models[capacity],
+                date: date,
+                pureAlcohol: pureAlcohol,
               })
-            }>
-            END PARTY
-          </ThemedButton>
+            }
+          />
         </View>
       </ScrollView>
     </SafeAreaView>
   );
 }
-const drinking = {
-  vodka: {
-    name: 'VODKA',
-    models: ['20ml', '40ml', '100ml', '0,5l', '1l'],
-  },
-  beer: {
-    name: 'BEER',
-    models: ['250ml', '300ml', '500ml', '1l'],
-  },
-  wine: {
-    name: 'WINE',
-    models: ['250ml', '300ml', '500ml', '1l'],
-  },
-};
 
 const styles = StyleSheet.create({
   container: {
